6c8cfb206d1919d1bba45bc552c52f2eee66bd02
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2019-10-30 20:24:19 +0530

    Add files via upload

--- Controller/MysqlController.js
+++ Controller/MysqlController.js
@@ -4,70 +4,85 @@ const bcrypt = require("bcrypt")
 let controller = {}
 
 // signup
-controller.signup = (req,res)=>{
+controller.signup = (req, res) => {
     try {
         console.log(req.body);
-        const {fname,lname,email,phone,pass} = req.body
+        const { fname, lname, email, phone, pass } = req.body
         // console.log(pass);
-        if(pass === "undefined" && email === "undefined"){
+        if (pass === "undefined" && email === "undefined") {
             res.redirect("/signup")
-        }else{
-            payload = {
-                name : {}
-            }
-            payload.name.first = fname;
-            payload.name.last = lname;
-            payload.email = email;
-            payload.phone = phone;
-            payload.pass = bcrypt.hashSync(pass, 10);;
+        } else {
 
-            const sql = `call Signup('${JSON.stringify(payload)}')`;
-            console.log(sql);
-            
-            mysql.query(sql, (err,result)=>{
-                if(err){
-                    console.log(err);
-                }else{
-                    res.redirect("/signin")
+            const sl = `select email from user where email = '${email}'`
+            // const sl = `select email,phone from user where email ='${email}' and phone = '${phone}'`
+            mysql.query(sl, (err, result) => {
+                if (result.length != 0) {
+                    res.send("Email aleady exixt")
+                    console.log("Email aleady exixt");
+                }
+                else {
+                    payload = {
+                        name: {}
+                    }
+                    payload.name.first = fname;
+                    payload.name.last = lname;
+                    payload.email = email;
+                    payload.phone = phone;
+                    payload.pass = bcrypt.hashSync(pass, 10);;
+
+                    const sql = `call Signup('${JSON.stringify(payload)}')`;
+                    console.log(sql);
+
+                    mysql.query(sql, (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            res.redirect("/signin")
+                        }
+                    })
                 }
             })
 
+
+
+
+
         }
-    // res.send("signup")
-    // res.redirect("/registration")
-        
+        // res.send("signup")
+        // res.redirect("/registration")
+
     } catch (error) {
         console.log(error.messege);
-        
+
     }
 }
 
 // Signin
-controller.signin = (req,res)=>{
+controller.signin = (req, res) => {
     try {
         // console.log(req.body);
-        
-        const {email,pass} = req.body
-        if(email === "undefined" && pass === "undefined"){
+
+        const { email, pass } = req.body
+        if (email === "undefined" && pass === "undefined") {
             res.redirect("/signin")
-        }else{
+        } else {
             const sql = `select password from user where email = '${email}'`;
-            mysql.query(sql,(err,result)=>{
-                if(result.length === "undefine"){
+            mysql.query(sql, (err, result) => {
+                if (result.length === "undefine") {
                     res.send("Please Enter Password")
                     console.log("Please Enter Password");
-                    
-                }else{
+
+                } else {
                     // console.log(result);
-                    
+
                     const isChack = bcrypt.compareSync(pass, result[0].password);
-                    if(!isChack){
+                    if (!isChack) {
                         res.send("Worng Password")
                         console.log("Worng Password");
-                        
-                    }else{
+
+                    } else {
                         console.log("Redirect to Wellcome page");
-                        
+
                         res.redirect("/main")
                     }
                 }
@@ -75,7 +90,7 @@ controller.signin = (req,res)=>{
         }
     } catch (err) {
         console.log(err.message);
-        
+
     }
 }
 
